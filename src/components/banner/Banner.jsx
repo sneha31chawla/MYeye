6880efac0492a631defa8d10caae0a0cef2ffa6f
sneha <snehachawla867@gmail.com>
2023-10-24 13:58:58 +0530

    fullproject

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,47 @@
+import { BsArrowDownRightCircle } from "react-icons/bs";
+
+import bannerImg from "../../assets/bannerImg.jpg";
+import { useNavigate } from "react-router";
+
+const Banner = ({ catRef }) => {
+  const navigate = useNavigate();
+
+  return (
+    <main className=" flex justify-between items-center py-1 mb-5  relative">
+      <section className="max-w-xl mx-auto sm:mx-0  w-full py-2  lg:w-1/3">
+        <h1 className="text-6xl  sm:text-7xl lg:text-8xl font-semibold  py-3 w-full ">
+        Frame & Focus
+        </h1>
+        <p className="py-3 text-md  text-gray-600">
+        Elevate Your Style with Premium Sunglasses.
+          <br />
+          A Century of Choices.
+        </p>
+        <section className="flex items-center">
+          <button
+            className="btn-primary text-sm md:text-base"
+            onClick={() => navigate("/products")}
+          >
+            Let's Shop
+          </button>
+          <button
+            className="p-3 flex items-center"
+            onClick={() =>
+              catRef.current.scrollIntoView({
+                behavior: "smooth",
+              })
+            }
+          >
+            <span className="mx-2 text-sm md:text-base">Uncover More</span>{" "}
+            <BsArrowDownRightCircle className="text-lg" />
+          </button>
+        </section>
+      </section>
+      <section className="hidden w-1/2 lg:flex justify-end">
+        <img src={bannerImg} alt="bannerImg" className="w-2/3 h-full" />
+      </section>
+    </main>
+  );
+};
+
+export default Banner;
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+  AiOutlineLinkedin,
+  AiFillGithub,
+  AiOutlineTwitter,
+} from "react-icons/ai";
+
+const Footer = () => {
+  return (
+    <div className="py-5 mt-3  bg-amber-50 flex flex-wrap justify-center items-center gap-2 md:gap-10  absolute right-0 left-0 ">
+      <p>MYeye made by Sneha Chawla </p>
+    </div>
+  );
+};
+
+export default Footer;
